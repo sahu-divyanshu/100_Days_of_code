38208010e0326a82a8fcfe36dcbe0b6f983faa48
divyanshu_sahu <divyanshusahucoc@gmail.com>
2022-07-22 23:17:00 +0530

    day_7

--- Day-005.cpp
+++ Day-005.cpp
@@ -1,4 +1,4 @@
-/*
+
 #include<iostream>
 using namespace std;
 int main (){
@@ -19,22 +19,30 @@ int main (){
 
 return 0;
 }
-*/
+
 #include<iostream>
 using namespace std;
 int main (){
+
     bool equal_reasult{};
     bool not_equal_reasult{};
 
     int num1{},num2{};
     
     cout << boolalpha; // will display true/false instead of 1/0 
+
     cout << "enter two integers" << endl;
+
     cin >> num1 >> num2;
+
     equal_reasult = (num1 == num2);
+
     not_equal_reasult = (num1 != num2);
+
     cout << "comparison reasult equals " <<equal_reasult <<endl;
+
     cout << "comparison reasult not equals " <<not_equal_reasult <<endl;
 
+    // this can be alsoo done with char and floats , char is compared with asky values
 return 0;
 }
