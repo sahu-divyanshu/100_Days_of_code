ed98265eaee471e759fbfde220eb128f60f8cc07
divyanshu_sahu <divyanshusahucoc@gmail.com>
2023-08-11 09:47:29 +0530

    day150

---
+++ web dev/random jokes/script.js
@@ -0,0 +1,16 @@
+const joke = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?type=single";
+
+let getJoke = () =>{
+   
+    fetch(url)
+    .then(data => data.json())
+    .then(item => {
+        joke.textContent = `${item.joke}`;
+        joke.classList.add("fade");
+    });
+}
+btn.addEventListener("click",getJoke);
+
+
